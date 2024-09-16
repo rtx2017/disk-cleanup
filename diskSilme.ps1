51dd5ca151ae767c92c0afbe595c8063acf2285d
feyza sazak <43961225+rtx2017@users.noreply.github.com>
2024-09-16 17:20:48 +0300

    Add files via upload

---
+++ diskSilme.ps1
@@ -0,0 +1,17 @@
+﻿$targetFolder = "C:\DCM"  # Hedef klasor
+$freeSpaceLimitGB = 55  # 50 GB limit
+$targetDate = (Get-Date).AddDays(-2)  # 2 gun oncesi tarih
+
+# C diskindeki bos alani kontrol et
+$drive = Get-PSDrive -Name C
+$freeSpaceGB = $drive.Free / 1GB
+
+# eger bos alan 50 GB'in altindaysa, 2 gun oncesinden eski dosyalari sil
+if ($freeSpaceGB -lt $freeSpaceLimitGB) {
+    Get-ChildItem -Path $targetFolder -Recurse -Force | Where-Object {
+        $_.LastWriteTime -lt $targetDate
+    } | Remove-Item -Force
+    Write-Host "eski dosyalar basariyla silindi."
+} else {
+    Write-Host "Bos alan yeterli, silme islemi yapılmadı."
+}
